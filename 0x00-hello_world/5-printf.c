78a35b5232581ac785b0f9a2a08a110c9a66eb63
Henry Davy Luc RULINDA <rulindaluc250@gmail.com>
2023-03-16 17:03:49 +0000

    printf commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+
 /**
- *main - prints the phrase with proper grammar
- *return: 0 if exited correctly
+ * main - prints the phrase with proper grammar
+ * Return: 0 if exited successfully
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
