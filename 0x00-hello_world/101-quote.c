7a2118e9150198d1f6a04de53051b226112612ec
Henry Davy Luc RULINDA <rulindaluc250@gmail.com>
2023-03-16 18:09:29 +0000

    quote and write

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - print quote
+ *
+ * Description: prints Dor's quote
+ *
+ * Return: always 1 (success)
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
