f17267f707974501ce9eb98d9814e19becb5daf1
Henry Davy Luc RULINDA <rulindaluc250@gmail.com>
2023-03-16 16:45:29 +0000

    puts commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
 /**
- * main - this is the main function
- * return: 0
+ * main - main block
+ * Return: 0
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
+
