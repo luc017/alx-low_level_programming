0a91142e4afc4e4b5f374ef41290b13bd56a82b3
Henry Davy Luc RULINDA <rulindaluc250@gmail.com>
2023-03-16 16:15:42 +0000

    puts commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ *
+ * return - alwzays 0
+ */
+
+int main (void)
+{
+	printf("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
