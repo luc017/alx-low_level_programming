5980cbb2a643d4b661e94efed5218014a9002f9a
Henry Davy Luc RULINDA <rulindaluc250@gmail.com>
2023-03-17 17:32:37 +0000

    for loop modified

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints all numbers
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	enum n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
