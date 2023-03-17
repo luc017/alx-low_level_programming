673127deae357e6adc827a64cc973c7017682ef0
Henry Davy Luc RULINDA <rulindaluc250@gmail.com>
2023-03-17 16:47:51 +0000

    putchar and if commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabetr in lowercase
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char a;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'e' && != 'q')
+		{
+			putchar(c);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
