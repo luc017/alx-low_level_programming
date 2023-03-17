cceff605c0755fa06effe2097cd43b1fbc683f63
Henry Davy Luc RULINDA <rulindaluc250@gmail.com>
2023-03-17 17:07:38 +0000

    for loop

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints digits
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d\n", i);
+	}
+
+	return (0);
+}
