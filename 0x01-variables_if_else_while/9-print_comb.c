f8473400f925685a6d10c0c89b01d14acf4ea2d5
Henry Davy Luc RULINDA <rulindaluc250@gmail.com>
2023-03-17 18:12:07 +0000

    combo commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - print single digit combination
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int k;
+
+	for (k = '0'; k <= '9'; k++)
+	{
+		putchar(k);
+
+		if (k != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
