aadb49a2d64ae7d77402553e51f03b92c0d36868
Henry Davy Luc RULINDA <rulindaluc250@gmail.com>
2023-03-17 17:48:39 +0000

    for loop modified too

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet in reverse
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char d;
+
+	for (d = 'z'; d <= 'a'; d--)
+	{
+		putchar(d);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
