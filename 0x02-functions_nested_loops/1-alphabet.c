e6a3f6aab175b9622282896e495cb14bc2a30865
Henry Davy Luc RULINDA <rulindaluc250@gmail.com>
2023-03-21 16:46:08 +0000

    alphabet lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabet in lowercase
+ */
+
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+		_putchar('a');
+
+	putchar('\n');
+}
