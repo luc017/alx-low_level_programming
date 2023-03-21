9f6d8f4233054becfb1ddb684d55fec3178580b1
Henry Davy Luc RULINDA <rulindaluc250@gmail.com>
2023-03-21 20:28:22 +0000

    adding integer

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - prints the result of addition
+ * @n: int to be added
+ * @m: int to be added to n
+ * Return: returns the result
+ */
+int add(int n, int m)
+{
+	int k;
+
+	k = n + m;
+
+	return (k);
+}
