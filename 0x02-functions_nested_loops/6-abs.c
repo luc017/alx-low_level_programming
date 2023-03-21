c77aa73f8fb8b7431224bb723c2487ff4afbca5a
Henry Davy Luc RULINDA <rulindaluc250@gmail.com>
2023-03-21 17:46:24 +0000

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - prints the absolute value of an intenger
+ * @n: the value to be analyse
+ * Return: the absolute value always
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
