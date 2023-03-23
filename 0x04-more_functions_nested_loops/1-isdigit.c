493a21e8c1e686e49b827ede65c7e7640e501a2e
Henry Davy Luc RULINDA <rulindaluc250@gmail.com>
2023-03-23 18:58:06 +0000

    digit or uppercase commit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isdigit - returns if it a digit or not
+ * @c: the parameter to be checked
+ * Return: 1 is C is a digit or 0 otherwise
+ */
+int _isdigit(int c)
+{
+	return (c >= 0 && c <= 9);
+}
