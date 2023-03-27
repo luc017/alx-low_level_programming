161f8c21dd0ff67ee114400dddf0a3783ef0cc4f
Henry Davy Luc RULINDA <rulindaluc250@gmail.com>
2023-03-27 21:20:09 +0000

    prints half of the string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of the string
+ * @str: pointer ot the string
+ */
+void puts_half(char *str)
+{
+	int i, half, n, len;
+
+	for (len = 0; str[len] != '\0'; len++)
+		;
+
+	if (len % 2 == 0)
+	{
+		half = len / 2;
+
+		for (i = half; i < len; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else
+	{
+		n = (len - 1) / 2;
+
+		for (i = n; i < len; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+}
