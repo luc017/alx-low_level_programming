eac09790fb9dc83dc2aad023df6a285afdb32c91
Henry Davy Luc RULINDA <rulindaluc250@gmail.com>
2023-03-27 16:06:19 +0000

    pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include <main.h>
+
+/**
+ * reset_to_98 - this points the value
+ * @n: pointer
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This readme is of the project regarding pointers, arrays and strings in c
