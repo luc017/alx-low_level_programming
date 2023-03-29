185078deb96768f4ec3c8720e6b2b3839904fca5
Henry Davy Luc RULINDA <rulindaluc250@gmail.com>
2023-03-29 17:39:36 +0000

    reverses an array of integerss

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * reverse_array - reverse an array of integers
+ * @a: pointer variable
+ * @n: variable
+ */
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int tp;
+
+	while (i < n / 2)
+	{
+		tp = a[i]
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = tp;
+		i++;
+	}
+}
