d81c423f68e1815a7c23d112c1232289a903b2b2
Henry Davy Luc RULINDA <rulindaluc250@gmail.com>
2023-03-29 15:18:49 +0000

    concentanating arrays

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,19 +9,18 @@
 char *_strcat(char *dest, char *src)
 {
 	int dest_len = 0;
+	int i = 0;
 
 	for (dest_len = 0; dest[dest_len] != '\0'; dest_len++)
+		;
+
+	while (src[i] != '\0')
 	{
-		int i = 0;
-
-		while (src[i] != '0')
-		{
-			dest[dest_len + i] = src[i]
-			i++;
-		}
-
-		dest[dest_len + i] = '\0'
+		dest[dest_len + i] = src[i];
+		i++;
 	}
+
+	dest[dest_len + i] = '\0';
 
 	return (dest);
 }
