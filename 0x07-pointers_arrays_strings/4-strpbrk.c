a35a3eba37336ef57e63fd462686435e6df3c50d
Henry Davy Luc RULINDA <rulindaluc250@gmail.com>
2023-04-03 16:17:48 +0000

    search for any set of bytes

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -13,9 +13,9 @@ char *_strpbrk(char *s, char *accept)
 	int d;
 	int k;
 
-	for (k = 0; accept[k] != '\0'; k++)
+	for (d = 0; s[d] != '\0'; d++)
 	{
-		for (d = 0; s[d] != '\0'; s++)
+		for (k = 0; accept[k] != '\0'; k++)
 		{
 			if (s[d] == accept[k])
 				return (s + d);
