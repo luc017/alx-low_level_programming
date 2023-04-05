dc108d39215c3c6bdb7c14e038971857e40beca2
Henry Davy Luc RULINDA <rulindaluc250@gmail.com>
2023-04-05 12:08:16 +0000

    palindrome check

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * is_palindrome - prints if a string is a palidrome or not
+ * @s: pointer to the string
+ *
+ * Return: 1 if a string, 0 otherwise
+ */
+
+int is_palindrome(char *s)
+{
+	if (s[0] == '\0')
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+}
