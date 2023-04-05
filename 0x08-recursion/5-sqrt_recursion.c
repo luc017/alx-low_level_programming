f678566b608f9cd77322f6edf32b04015e776811
Henry Davy Luc RULINDA <rulindaluc250@gmail.com>
2023-04-05 18:23:46 +0000

    square root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,18 @@
 #include "main.h"
 /**
+ * _sqrt - calculates the square root of n
+ * @v: value to use while working out the square root
+ * @rt: the root
+ *
+ * Return: square root
+ */
+int _sqrt(int v, int rt)
+{
+	if (v * v == rt)
+		return (v);
+	return (_sqrt(v + 1, rt));
+}
+/**
  * _sqrt_recursion - returns the square root
  * @n: integer to be operated on
  *
@@ -9,23 +22,9 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-
 	if (n == 0 || n == 1)
 	{
 		return (n);
 	}
 	return (_sqrt(1, n));
-}
-/**
- * _sqrt - calculates the square root of n
- * @v: value to use while working out the square root
- * @rt: the root
- *
- * Return: square root
- */
-int _sqrt(int v, int rt)
-{
-	if (v * v == root)
-		return (v);
-	return (_sqrt(v + 1, rt));
 }
