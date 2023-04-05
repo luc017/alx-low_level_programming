ea1a1b22625c33a58547c5de6e45efcc913860f2
Henry Davy Luc RULINDA <rulindaluc250@gmail.com>
2023-04-05 16:54:01 +0000

    length of a string

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,4 +1,4 @@
-#define "main.h"
+#include "main.h"
 /**
  * _print_rev_recursion - prints a string in reverse
  * @s: pointer to the string
@@ -8,7 +8,7 @@ void _print_rev_recursion(char *s)
 {
 	if (*s > '\0')
 	{
-		_print_rev_recursion(char *s)
-		_putchar(*s)
+		_print_rev_recursion(char *s);
+		_putchar(*s);
 	}
 }
---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: pointer to the string
+ *
+ * Return: length
+ */
+int _strlen_recursion(char *s)
+{
+	int length;
+
+	if (*s == '\0')
+		return (0);
+	return (_strlen_recursion(length + (s + 1)));
+}
