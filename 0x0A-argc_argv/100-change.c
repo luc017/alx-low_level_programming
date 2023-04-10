6ee7d9b5e38a8c19485e450e67db13f51ed126e7
Henry Davy Luc RULINDA <rulindaluc250@gmail.com>
2023-04-10 19:43:56 +0000

    change commit

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -14,23 +14,20 @@ int main (int argc, char **argv)
 	int i = 0;
 	int c_value[] = {25, 10, 5, 2, 1};
 
-	if (argc == 2)
-	{
-		cents = atoi(argv[1]);
-
-		for (i = 0; i < 5; i++)
-		{
-			c += cents / c_value[i];
-			cents = cents % c_value[i];
-		}
-
-		printf("%d\n", c);
-	}
-	else
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	cents = atoi(argv[1]);
+
+	for (i = 0; i < 5; i++)
+	{
+		c += cents / c_value[i];
+		cents = cents % c_value[i];
+	}
+
+	printf("%d\n", c);
 
 	return (0);
 }
