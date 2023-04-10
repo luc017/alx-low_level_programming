6d3d3e178ec7d39f4b06e68f32050842a454cd50
Henry Davy Luc RULINDA <rulindaluc250@gmail.com>
2023-04-10 16:36:54 +0000

    argc commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the number of arguments
+ * @argc: number of arguments entered'
+ * @argv: variables entered
+ *
+ * Return: 0 (success)
+ */
+int main(int argc, char **argv)
+{
+	printf("%d\n", argc);
+
+	(void)argv;
+
+	return (0);
+}
