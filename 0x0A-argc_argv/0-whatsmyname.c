688217402d84e2b9d8fe342f2dfae7647b435947
Henry Davy Luc RULINDA <rulindaluc250@gmail.com>
2023-04-10 16:24:39 +0000

    argv[0] commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints out the name of program
+ * @argc: number of arguments
+ * @argv: pointer to the array
+ *
+ * Return: 0 (success)
+ */
+int main(int argc, char **argv)
+{
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
