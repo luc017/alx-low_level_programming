22153363482b38d1707da9563b5deb57222913f8
Henry Davy Luc RULINDA <rulindaluc250@gmail.com>
2023-04-11 18:24:23 +0000

    concatenating strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,16 +11,25 @@
 char *str_concat(char *s1, char *s2)
 {
 	int len_s1, len_s2, len;
-	int i;
+	int d;
 	char *ptr;
 
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
 
 	for (len_s1 = 0; s1[len_s1] != '\0'; len_s1++)
 		;
 	for (len_s2 = 0; s2[len_s2] != '\0'; len_s2++)
 		;
 
-	len = len_s1 + len_s2 + 1;
+	len = (len_s1 + len_s2) + 1;
 
 	ptr = (char *)malloc(len * sizeof(char));
 
@@ -28,12 +37,16 @@ char *str_concat(char *s1, char *s2)
 	{
 		return (NULL);
 	}
-	for (i = 0; i < len_s2; i++)
+
+	for (d = 0; d < len_s1; d++)
 	{
-		s1[len_s1 + i] = s2[i];
-		ptr[i] = s1[len_s1 + i];
+		ptr[d] = s1[d];
 	}
-	ptr[i] = '\0';
+	for (d = 0; d < len_s2; d++)
+	{
+		ptr[len_s1 + d] = s2 [d];
+	}
+	ptr[len - 1] = '\0';
 
 	return (ptr);
 }
