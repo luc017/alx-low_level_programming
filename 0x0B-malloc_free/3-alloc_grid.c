ba11f3139489414383b18545a2caba6664ef9beb
Henry Davy Luc RULINDA <rulindaluc250@gmail.com>
2023-04-11 20:53:42 +0000

    grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * alloc_grid - allocates the mem. to the grid
+ * @width: columns of the grid
+ * @height: rows of the grid
+ *
+ * Return: the array, NULL otherwise
+ */
+int **alloc_grid(int width, int height)
+{
+	int a, b;
+	int  **ptr;
+
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	ptr = (int **)malloc(height * sizeof(int *));
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (a = 0; a < height; a++)
+	{
+		ptr = (int **)malloc(width * sizeof(int *));
+	}
+	if (ptr == NULL)
+	{
+		for (b = 0; b < a; b++)
+		{
+			free(ptr[b]);
+		}
+		free(ptr);
+		return (NULL);
+	}
+	for (b = 0; b <= a; b++)
+	{
+		ptr[a][b] = 0;
+	}
+
+	return (ptr);
+
+}
---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_grid - frees the frid or integered
+ * @grid: width
+ * @height: height
+ *
+ * Return: 
