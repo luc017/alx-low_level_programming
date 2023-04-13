483fe7015f77bbf54f339770c33efc9b306ac913
Henry Davy Luc RULINDA <rulindaluc250@gmail.com>
2023-04-13 17:39:45 +0000

    realloc

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _realloc - reallocates memory
+ * @ptr: pointer
+ * @old_size: the size of the last memo
+ * @new_size: the new size
+ *
+ * Return: pointer to the new memo
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *p;
+	char *new_ptr;
+	unsigned int b;
+
+	new_ptr = ptr;
+	if (old_size > new_size)
+		old_size = new_size;
+
+	else if (new_size == old_size)
+		return (ptr);
+
+	else if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	else
+	{
+
+		if (ptr == NULL)
+		{
+			ptr = malloc(new_size);
+			if (ptr == NULL)
+				return (NULL);
+			return (ptr);
+		}
+		p = malloc(new_size);
+
+		for (b = 0; b < old_size; b++)
+		{
+			p[b] = new_ptr[b];
+		}
+
+	}
+	free(ptr);
+	return (p);
+}
