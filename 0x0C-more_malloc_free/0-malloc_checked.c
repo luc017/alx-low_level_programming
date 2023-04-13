49df25607bf8c753a9a9686477ca76fc7c877e48
Henry Davy Luc RULINDA <rulindaluc250@gmail.com>
2023-04-13 12:25:07 +0000

    creates space for malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * malloc_checked - it allocates memory using malloc
+ * @b: integer
+ *
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(sizeof(char) * b);
+
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+
+	return (ptr);
+}
