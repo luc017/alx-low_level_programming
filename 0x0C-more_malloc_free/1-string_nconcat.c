a0e71cf0e889fa044505cafa1a08774380a02b9d
Henry Davy Luc RULINDA <rulindaluc250@gmail.com>
2023-04-13 13:43:44 +0000

    string concatenation

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,17 +11,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ptr;
 	unsigned int len_s1, len_s2, m;
-	int len = 0;
 
-	/*calculating the length of s1 and s2*/
-	for (len_s1 = 0; s1[len_s1] != '\0'; len_s1++)
-		;
-	for (len_s2 = 0; s2[len_s2] != '\0'; len_s2++)
-		;
-
-	len = len_s1 + len_s2 + 1;
-
-	/*for the case where NULL is the input*/
 	if (s1 == NULL)
 	{
 		s1 = "";
@@ -30,44 +20,28 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		s2 = "";
 	}
+	for (len_s1 = 0; s1[len_s1] != '\0'; len_s1++)
+		;
+	for (len_s2 = 0; s2[len_s2] != '\0'; len_s2++)
+		;
 
-	if (n < len_s2)
+	if(n >= len_s2)
+		n = len_s2;
+
+	ptr = malloc(sizeof(char) * (len_s1 + n) + 1);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (m = 0; m < len_s1; m++)
 	{
-		ptr = malloc(sizeof(char) * (len_s1 + n) + 1);
-
-		if (ptr == NULL)
-		{
-			return (NULL);
-		}
-
-		for (m = 0; m < len_s1; m++)
-		{
-			ptr[m] = s1[m];
-		}
-		for (m = 0; m < n; m++)
-		{
-			ptr[len_s1 + m] = s2[m];
-		}
-		ptr[len_s1 + n] = '\0';
+		ptr[m] = s1[m];
 	}
-	else
+	for (m = 0; m < n; m++)
 	{
-		ptr = malloc(len * sizeof(char));
-
-		if (ptr == NULL)
-		{
-			return (NULL);
-		}
-		for (m = 0; m < len_s1; m++)
-		{
-			ptr[m] = s1[m];
-		}
-		for (m = 0; m < len_s2; m++)
-		{
-			ptr[len_s1 + m] = s2[m];
-		}
-		ptr[len - 1] = '\0';
+		ptr[len_s1 + m] = s2[m];
 	}
+	ptr[m] = '\0';
 
 	return (ptr);
 }
