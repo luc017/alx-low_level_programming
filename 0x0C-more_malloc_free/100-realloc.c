d5a4d41198e44de6bd81d5ebf1cb006e5325a499
Henry Davy Luc RULINDA <rulindaluc250@gmail.com>
2023-04-13 18:01:50 +0000

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -16,7 +16,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	unsigned int b;
 
 	new_ptr = ptr;
-	if (old_size > new_size)
+	if (new_size > old_size)
 		old_size = new_size;
 
 	if (new_size == old_size)
