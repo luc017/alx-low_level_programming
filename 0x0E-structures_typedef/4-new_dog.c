47714399e8103a198bfae55425c49da73a6fa927
Henry Davy Luc RULINDA <rulindaluc250@gmail.com>
2023-04-17 21:30:13 +0000

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,19 +1,50 @@
 #include "dog.h"
-#include <stdio.h>
+#include <stdlib.h>
 /**
- * new-dog - new dog's data
+ * new_dog - new dog's data
  * @name: name of that dog
  * @age: the age of the dog
  * @owner: name of the dog
  *
- * Return: NULL if func fails, 0 success
+ * Return: pointer to the new structure, NULL otherwise
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	if (new_dog  == NULL)
-		return;
+	int nlen, ownlen, b;
+	dog_t *dog;
 
-	new_dog->name = name;
-	new_dog->age = age;
-	new_dog-> = owner;
+	dog = malloc(sizeof(dog_t));
+	if (dog  == NULL)
+		return (NULL);
+
+	for (nlen = 0; name[nlen] != '\0'; nlen++)
+		;
+	for (ownlen = 0; name[ownlen] != '\0'; ownlen++)
+		;
+
+	dog->name = malloc(nlen * sizeof(dog->name));
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	while (b <= nlen)
+	{
+		dog->name[b] = name[b];
+		b++;
+	}
+	dog->age = age;
+	dog->owner = malloc(ownlen * sizeof(dog->owner));
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	while (b <= ownlen)
+	{
+		dog->owner[b] = owner[b];
+		b++;
+	}
+	return (dog);
 }
