f1dc13d50b7cf4fb4961f59725ffc08a8213fdcd
Henry Davy Luc RULINDA <rulindaluc250@gmail.com>
2023-04-19 13:55:17 +0000

    action on int

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -15,6 +15,6 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	if (array == NULL || action == NULL)
 		return;
 
-	for (d = 0; d <= size; d++)
+	for (d = 0; d < size; d++)
 		action(array[d]);
 }
