039961d75778266aa61d02ef12efe1591c2573cd
Henry Davy Luc RULINDA <rulindaluc250@gmail.com>
2023-04-20 13:51:40 +0000

    print string

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -17,16 +17,14 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(p, n);
 	for (d = 0; d < n; d++)
 	{
-		if (d != 0 && separator != NULL)
-		{
-			printf("%s", separator);
-		}
-
 		value = va_arg(p, char *);
 		if (value == NULL)
 			printf("(nil)");
+		else
+			printf("%s", va_arg(p, char *));
 
-		printf("%s", va_arg(p, char *));
+		if (d != 0 && separator != NULL)
+			printf("%s", separator);
 	}
 	printf("\n");
 
