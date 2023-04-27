ca452a071b5e59ad4bf482d79d244c73173ad1a5
Henry Davy Luc RULINDA <rulindaluc250@gmail.com>
2023-04-27 12:46:17 +0000

    length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include "lists"
+#include <stdlib.h>
+/**
+ * list_len - returns the number of elements in the list
+ * @h: pointer to the list
+ *
+ * Return: number of elements in the list
+ */
+size_t list_len(const list_t *h)
+{
+	unsigned int l = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			return (0);
+
+		else
+			l++;
+		h->next;
+	}
+	return (l);
+}
