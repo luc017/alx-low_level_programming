09ad52d2f8b2dd66f2e8056a3e4c0b90ebea03c3
Henry Davy Luc RULINDA <rulindaluc250@gmail.com>
2023-05-01 17:25:52 +0000

    inserting a node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,7 +24,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	temporary = *head;
 	while (temporary != NULL && counter < idx - 1)
 	{
-		if (counter == idx)
+		if (counter == idx - 1)
 		{
 			new->next = temporary->next;
 			temporary->next = new;
