b993bac896f27a512f252974e45b5c93a58005cd
Henry Davy Luc RULINDA <rulindaluc250@gmail.com>
2023-05-01 18:40:12 +0000

    delete a node

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -26,6 +26,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	while (temporary != NULL && counter < index)
 	{
+		prev = temporary;
 		temporary = temporary->next;
 		counter++;
 	}
@@ -33,8 +34,6 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (temporary == NULL)
 		return (-1);
 
-	prev = temporary;
-	prev = prev->next;
 	prev->next = temporary->next;
 	free(temporary);
 
