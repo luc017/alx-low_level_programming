bfffcdf29d502718b960af72943e1a01f021a06c
Henry Davy Luc RULINDA <rulindaluc250@gmail.com>
2023-05-01 17:28:48 +0000

    inserting a node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,6 +21,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new->n = n;
 	new->next = NULL;
 
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+
 	temporary = *head;
 	while (temporary != NULL && counter < idx - 1)
 	{
@@ -33,11 +40,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		temporary = temporary->next;
 		counter++;
 	}
-	if (idx == 0)
-	{
-		new->next = *head;
-		*head = new;
-		return (new);
-	}
+
 	return (NULL);
 }
