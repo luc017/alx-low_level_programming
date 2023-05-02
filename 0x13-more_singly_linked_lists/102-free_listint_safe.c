3abbc9f69e914d819ce3c0a162e2866cca457afe
Henry Davy Luc RULINDA <rulindaluc250@gmail.com>
2023-05-02 13:24:11 +0000

    free up memory

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -10,15 +10,13 @@ size_t free_listint_safe(listint_t **h)
 {
 	size_t counter = 0;
 	listint_t *temporary;
-	int num;
 
 	if (h == NULL || *h == NULL)
 		return (0);
 
 	while (*h != NULL)
 	{
-		num = *h - (*h)->next;
-		if (num > 0)
+		if ((*h)->next >= *h)
 		{
 			temporary = (*h)->next;
 			free(*h);
