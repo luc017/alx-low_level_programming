d2cfc6a2469c6bcc5490962ef1770ae89dea285e
Henry Davy Luc RULINDA <rulindaluc250@gmail.com>
2023-05-08 21:15:40 +0000

    cp

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -8,11 +8,19 @@
 
 #define ELF_MAGIC "\177ELF"
 
+/**
+ * print_error - prints the error
+ * @message: pointer
+ */
 void print_error(const char *message)
 {
 	fprintf(stderr, "%s\n", message);
 	exit(98);
 }
+/**
+ * print_elf_header - prints the header
+ * @ehdr: pointer
+ */
 void print_elf_header(const Elf64_Ehdr *ehdr)
 {
 	int d;
@@ -22,11 +30,22 @@ void print_elf_header(const Elf64_Ehdr *ehdr)
 		printf("%02x ", ehdr->e_ident[d]);
 	printf("\n");
 
-	printf("  Class:                             %s\n", ehdr->e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" : "ELF64");
-	printf("  Data:                              %s\n", ehdr->e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" : "2's complement, big endian");
-	 printf("  Version:                           %d%s\n", ehdr->e_ident[EI_VERSION], ehdr->e_ident[EI_VERSION] == EV_CURRENT ? " (current)" : "");
-	 printf("  OS/ABI:                            %s\n", ehdr->e_ident[EI_OSABI] == ELFOSABI_SYSV ? "UNIX - System V" : "Other");
-	 printf("  ABI Version:                       %d\n", ehdr->e_ident[EI_ABIVERSION]);
+	printf("  Class:                             %s\n",
+			ehdr->e_ident[EI_CLASS] == ELFCLASS32 ?
+			"ELF32" : "ELF64");
+	printf("  Data:                              %s\n",
+			ehdr->e_ident[EI_DATA] == ELFDATA2LSB ?
+			"2's complement, little endian" :
+			"2's complement, big endian");
+	 printf("  Version:                           %d%s\n",
+			 ehdr->e_ident[EI_VERSION],
+			 ehdr->e_ident[EI_VERSION] == EV_CURRENT ?
+			 " (current)" : "");
+	 printf("  OS/ABI:                            %s\n",
+			 ehdr->e_ident[EI_OSABI] == ELFOSABI_SYSV ?
+			 "UNIX - System V" : "Other");
+	 printf("  ABI Version:                       %d\n",
+			ehdr->e_ident[EI_ABIVERSION]);
 	 printf("  Type:                              ");
 
 	 switch (ehdr->e_type)
@@ -52,6 +71,11 @@ void print_elf_header(const Elf64_Ehdr *ehdr)
 	 }
 	 printf("  Entry point address:               %lx\n", ehdr->e_entry);
 }
+/**
+ * main - executes the elf
+ * @argc: argument count
+ * @argv: argument vector
+ */
 int main (int argc, char **argv)
 {
 	int file;
